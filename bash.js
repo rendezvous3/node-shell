fe67cfde23e33b59279930fead95c6aa1e6ca615
Bojan Jovanovic <bojanj6m@gmail.com>
2017-08-02 00:39:03 -0400

    finished

--- bash.js
+++ bash.js
@@ -2,24 +2,30 @@ const chalk = require('chalk');
 const commands = require('./commands');
 
 const prompt = chalk.blue('\nprompt > ');
+
+var cmdGroups = [];
+
 // Output a prompt
-//const prompt = process.stdout.write('\nprompt > ');
 process.stdout.write(prompt);
 
 process.stdin.on('data', function(data){
-    //var cmd = data.toString().trim(); // remove the newline
+    cmdGroups = data.toString().trim().split(/\s*\|\s*/g);
+    execute(cmdGroups.shift());
+});
 
-    const tokens = data.toString().trim().split(' ');
+function execute(cmdString, lastOutput) {
+    const tokens = cmdString.toString().trim().split(' ');
     const cmd = tokens[0];
     const args = tokens.slice(1).join(' ');
 
-    if (commands[cmd]) commands[cmd](args, done);
-    else if(commands[cmd]) commands[cmd](args, done);
-    else process.stderr.write('command not found: ' + cmd);
-    // process.stdout.write('\nprompt > ');
-});
+    if (commands[cmd]) commands[cmd](lastOutput, args, done);
+}
 
 function done (output) {
-    process.stdout.write(output +  prompt);
+    if(cmdGroups.length) {
+        execute(cmdGroups.shift(), output);
+    } else {
+        process.stdout.write(output + prompt)
+    }
 }
 
--- commands/optional.js
+++ commands/optional.js
@@ -2,82 +2,98 @@
 
 const fs = require('fs');
 const request = require('request');
+const handleErr = require('../utils').handleErr;
 
-function cat(filenames, done) {
-    filenames = filenames.split(' ');
-    const texts = [];
-    var count = 0;
-    filenames.forEach(function(filename, i) {
-        fs.readFile(filename, {encoding: 'utf8'}, function (err, text){
-            if(err) throw err;
-            texts[i] = text;
-            count++;
-            if(count === filenames.length) {
-                done(texts.join(''));
-            }
-        });
-    });
-}
+/*----------- Functions that may take STDIN --------------*/
 
-function head (filename, done) {
+function cat (stdin, filenames, done) {
+  if (stdin && !filenames) return done(stdin); // `return` stops fn; could use else instead.
+  filenames = filenames.split(' ');
+  const texts = []; // const means no reassignment, but obj can be mutated
+  var count = 0;
+  filenames.forEach(function (filename, i) {
     fs.readFile(filename, { encoding: 'utf8' }, function (err, text) {
-        if (err) throw err;
-        done(text.split("\n").slice(0,5).join('\n'));
-    })
+      if (err) handleErr(err);
+      if (!text) text = '';
+      texts[i] = text;
+      count++;
+      if (count === filenames.length) done(texts.join('')); // like Unix: no newline. Dangerous!
+    });
+  });
 }
 
-function tail (filename, done) {
-    fs.readFile(filename, { encoding: 'utf8' }, function(err, text){
-        if (err) throw err;
-        done(text.split('\n').slice(-5).join('\n'));
-    })
+// commands that work on a single file:
+
+// This helps to DRY up the following five functions
+function processFile (stdin, filename, done, custom) {
+  if (stdin && !filename) produceOutput(null, stdin);
+  else fs.readFile(filename, {encoding: 'utf8'}, produceOutput);
+  function produceOutput (err, text) {
+    if (err) handleErr(err);
+    if (!text) text = '';
+    done(custom(String(text)));
+  }
 }
 
-function sort (filename, done) {
-    fs.readFile(filename, { encoding: 'utf8' }, function(err, text){
-        if (err) throw err;
-        done(text.split('\n').sort().join('\n'));
-    })
+function head (stdin, filename, done) {
+  processFile(stdin, filename, done, function custom (text) {
+    return text.split('\n').slice(0, 5).join('\n');
+  });
 }
 
-function wc (filename, done) {
-    fs.readFile(filename, { encoding: 'utf8' }, function(err, text){
-        if (err) throw err;
-        done(text.split('\n').length);
-    })
+function tail (stdin, filename, done) {
+  processFile(stdin, filename, done, function custom (text) {
+    return text.split('\n').slice(-5).join('\n');
+  });
 }
 
-function uniq (filename, done) {
-    fs.readFile(filename, { encoding: 'utf8' }, function(err, text){
-        if (err) throw err;
-        const lines = text.split('\n');
-        for(var i=0; i < lines.length; i++) {
-            if(lines[i] === lines[i+1]) {
-                lines.splice(i, 1);
-                i--;
-            }
-        }
-        done(lines.join('\n'));
-    })
+function sort (stdin, filename, done) {
+  processFile(stdin, filename, done, function custom (text) {
+    return text.split('\n').sort().join('\n');
+  });
 }
 
-function curl(url, done) {
-    if (url.slice(0, 7) !== 'http://') url = 'http://' + url;
-    request(url, function(err, response, body){
-        if(err) throw err;
-        else if (response && (response.statusCode > 399)) throw new Error(response.statusCode);
-        if(body) done(body);
-        else done('');
-   })
+function wc (stdin, filename, done) {
+  processFile(stdin, filename, done, function custom (text) {
+    return text.split('\n').length;
+  });
 }
 
+function uniq (stdin, filename, done) {
+  processFile(stdin, filename, done, function custom (text) {
+    const lines = text.split('\n');
+    for (var i = 0; i < lines.length - 1; i++) {
+      if (lines[i] === lines[i + 1]) {
+        lines.splice(i, 1);
+        i--; // have to check this line for duplicates again
+      }
+    }
+    return lines.join('\n');
+  });
+}
 
+// commands that work on a url:
+
+function curl (stdin, url, done) {
+  if (stdin && !url) return produceOutput(null, {statusCode: 200}, stdin);
+  // curl does an HTTP GET request and outputs the response body
+  if (url.slice(0, 7) !== 'http://') url = 'http://' + url;
+  request(url, produceOutput);
+  function produceOutput (err, response, body) {
+    if (err) handleErr(err);
+    else if (response && (response.statusCode > 400)) handleErr(response.statusCode);
+    if (body) done(body);
+    else done('');
+  }
+}
+
+// "revealing module" pattern: can see API at a glance, but have to maintain
 module.exports = {
-    cat:cat,
-    head: head,
-    tail: tail,
-    sort: sort,
-    wc: wc,
-    uniq: uniq,
-    curl: curl
-}
+  cat: cat,
+  head: head,
+  tail: tail,
+  sort: sort,
+  wc: wc,
+  uniq: uniq,
+  curl: curl
+};
